65dfd790980cc5a0cb2e0745dcb6ea722755fc51
Tom <tommusique974@gmail.com>
2024-02-25 16:59:13 +0100

    push doctolib

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.common.by import By
 import csv 
-# from sites.amazon import check_amazon_registration
 from sites.doctolib import check_doctolib_registration
-
 
 def setup_driver():
     opt = Options()
@@ -16,20 +13,16 @@ def setup_driver():
     return driver
 
 def main(email_list):
-    driver = setup_driver()
     results = []
 
     for email in email_list:
-    
-        
-        # Vérifiez l'inscription sur Doctolib
+        driver = setup_driver() # Créez une nouvelle instance du navigateur pour chaque email
         doctolib_registered = check_doctolib_registration(driver, email)
-        
+        print(doctolib_registered)
+        driver.quit() # Fermez le navigateur après chaque vérification
         # Ajouter le résultat à la liste
         results.append([email, doctolib_registered])
 
-    driver.quit()
-    
     # Exporter les résultats en CSV
     with open('resultats_inscription.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
--- sites/doctolib.py
+++ sites/doctolib.py
@@ -1,17 +1,23 @@
 import time
 from selenium.webdriver.common.by import By
+import random
+from selenium.common.exceptions import NoSuchElementException
+
+
+
+number_list= ['0628436936','0611739434','0738168474','0754657472', '0613763374', '0699656374']
 
 def check_doctolib_registration(driver, email):
     driver.get("https://www.doctolib.fr/sessions/new?context=navbar")
+    time.sleep(2)
     driver.find_element(By.XPATH, '//*[@id="didomi-notice-agree-button"]').click()
-    nowUrl = driver.current_url
-    # if nowUrl == ""
-    driver.find_element(By.CLASS_NAME,'.dl-button-primary').click()
-    #driver.find_element(By.CSS_SELECTOR,'.dl-button-tertiary-primary > span:nth-child(1)').click()
+
+    driver.find_element(By.CSS_SELECTOR,'.dl-button-tertiary-primary > span:nth-child(1)').click()
+
    
     #phone
     phone_input = driver.find_element(By.ID, 'phone_number')
-    phone_input.send_keys("0683042938") #phone factice
+    phone_input.send_keys(random.choice(number_list)) #phone factice
     
     #email
     email_input = driver.find_element(By.ID, 'email')
@@ -32,11 +38,29 @@ def check_doctolib_registration(driver, email):
     cbpgu = driver.find_element(By.NAME,'cgu')
     cbpgu.click()
     
-    cbru = driver.find_element(By.NAME,'remember_username')
-    cbru.click()
+    # cbru = driver.find_element(By.NAME,'remember_username')
+    # cbru.click()
+    driver.find_element(By.CSS_SELECTOR,'.dl-button-primary > span:nth-child(1)').click()
+    time.sleep(2)
     
-    driver.find_element(By.CLASS_NAME,'dl-button-label').click()
-
+    driver.find_element(By.CSS_SELECTOR, ".dl-modal-footer > button:nth-child(2)").click()
     
-    time.sleep(1000000)
+    time.sleep(1)
+    result = ""
+    current_url = driver.current_url
+    if current_url == "https://www.doctolib.fr/account/marketing_consent/new":
+        result = "False"
+        print("ok")
+    else:
+        print("nonn")
+        span_element = driver.find_element(By.CSS_SELECTOR, "span.dl-text-body.dl-text-regular.dl-text-s")
+        text_recup = span_element.text
+        
+        if text_recup == "Désolé, une erreur est survenue." or text_recup == "Un compte Doctolib a déjà été créé avec ce numéro de téléphone.":
+            result = "False"
+        elif text_recup == "Un compte Doctolib a déjà été créé avec cette adresse e-mail." or text_recup == "Un compte Doctolib a déjà été créé avec cette adresse e-mail. Un compte Doctolib a déjà été créé avec ce numéro de téléphone.":
+            result = "True"
+              
+    return result
+        
     
